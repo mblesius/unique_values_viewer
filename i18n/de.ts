a0a93138c7f49e49b3a252f5c30be8f74290ecd3
Malik Blesius <78353871+mblesius@users.noreply.github.com>
2023-07-01 12:44:53 +0200

    Create de.ts

---
+++ i18n/de.ts
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="de_DE">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../test.py" line="7"/>
+        <source>File</source>
+        <translation>Datei</translation>
+    </message>
+    <message>
+        <location filename="../test.py" line="8"/>
+        <source>Hello World</source>
+        <translation>Hallo Welt</translation>
+    </message>
+</context>
+</TS>
+
+
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="af" sourcelanguage="en">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="test_translations.py" line="48"/>
+        <source>Good morning</source>
+        <translation>Goeie more</translation>
+    </message>
+</context>
+</TS>
